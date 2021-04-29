11327513936a492385113b41647aea94ec6c19f6
lorenzo torlontano <lorenzo.torlontano@gmail.com>
2021-04-29 17:30:59 +0200

    cleaning more

--- src/components/ShoppingCart/ShoppingCartStyle.tsx
+++ src/components/ShoppingCart/ShoppingCartStyle.tsx
@@ -2,57 +2,71 @@ import styled from "styled-components";
 
 export const Button = styled.span`
   text-decoration: underline;
+  text-overflow: inherit;
   cursor: pointer;
 `;
 
 export const WrapperShoppingCartComponent = styled.div`
   background-color: #eaeded;
-  margin: 30px;
-  padding: 30px;
+  height: 100vh;
+  padding: 10px;
+`;
+
+export const CardShoppingCart = styled.div`
+  background-color: white;
+  height: 100%;
+  padding: 20px;
+  max-width: 75%;
+  margin: 10px;
+`;
+
+export const SpanText = styled.span`
+  font-size: "30px";
+  text-transform: "capitalize";
+  font-weight: "bold";
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const SeparateProductLine = styled.div`
+  min-width: "1px";
+  background-color: "gray";
+  min-height: "18px";
+  margin-left: "5px";
+  margin-right: "5px";
 `;
 
 export const SepareteLine = styled.div`
-  background-color: black;
   height: 1px;
-  margin-bottom: 10px;
+  background-color: gray;
+`;
+
+export const DescriptionProduct = styled.div`
+  color: green;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
 `;
 
 export const WrapperList = styled.div`
+  padding: 10px;
   display: flex;
+  align-items: center;
 `;
 
-export const WrapperItem = styled.div``;
-
-export const ImageList = styled.img``;
+export const ContainerTitle = styled.div`
+  font-weight: bold;
+  text-transform: uppercase;
+`;
 
 export const ContainerIcon = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  width: 115px;
-`;
-
-export const ContainerButtonRemoveAll = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin: 10px;
-`;
-
-export const ContainerTitle = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const ContainerItemCart = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-export const WrapperPrice = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 300px;
+  width: 380px;
+  margin-top: 10px;
 `;
